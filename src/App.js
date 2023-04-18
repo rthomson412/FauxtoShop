33f2e88e8591621f141b8a49a4705a0ff6987b44
Ross Thomson <rthomson412@gmail.com>
2023-04-18 13:46:43 -0400

    css basic

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Slider from './Slider';
 function App() {
   return (
     <div className='container'>
-      <div className='main-image'/>
+      <div className='main-image'>IMAGE</div>
       <div className='sidebar'>
-        
+        SIDEBAR
       </div>
       <Slider/>
     </div>
